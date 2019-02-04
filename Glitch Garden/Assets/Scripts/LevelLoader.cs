1ed54ffeef094c5a9f6702f976b70e43d321bb4c
dstrauss <fmdenver@gmail.com>
2019-02-04 13:33:41 -0500

    initial commit

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    int currentSceneIndex;
+    [SerializeField] int waitInSeconds = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadTitle());
+        }
+ 
+    }
+
+    IEnumerator LoadTitle()
+    {
+        yield return new WaitForSeconds(waitInSeconds);
+        LoadNextScene();
+        //SceneManager.LoadScene("Title Screen");
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+}
